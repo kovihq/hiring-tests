d22b89b6facd7d98e8e062a3784a32fe9f2722bf
Fagner <bfagner@hotmail.com>
2019-07-03 22:28:55 -0300

    solution

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ function intersection (array1, array2) {
   console.log(array1.filter(value => array2.indexOf(value) !== -1).sort((a,b) => a - b))
 }
 
-
 intersection([3, 5, 6, 1, 2, 16], [16, 6, 91, 1, 4, 3, 123, 1, 1])
