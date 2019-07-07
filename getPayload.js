cc77c5b364f4b218c90efe37be0cc31468fae9f6
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-07-07 18:35:50 -0300

    Adds comment.

--- getPayload.js
+++ getPayload.js
@@ -1,5 +1,8 @@
 const { intersection } = require('lodash/array')
 
 module.exports = (arr1, arr2) => {
+  /* Gets common numbers between two arrays, using lodash's intersection.
+     Sorts the result using javascript sort function.
+     And returns it as an array */
   return intersection(arr1, arr2).sort((a, b) => a - b)
 }
