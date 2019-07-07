3be75095c6c10d711f70284782456db1026de78d
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-07-07 18:46:03 -0300

    Creates POST endpoint to receive the arrays.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
+const getPayload = require('./getPayload')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.post('/', (req, res) => {
+  const response = getPayload(req.body.arr1, req.body.arr2) // Gets the arrays from the message body, sents them to getPayload function and gets the response
+  res.json({ response: response }) // Returns the response to the client in json format
 })
 
 app.listen(port, () => {
