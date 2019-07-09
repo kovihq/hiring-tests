a27fea3ac2c32b3355d3a7057517fab186e59d97
Vitor Garcia <vitfgarcia@gmail.com>
2019-07-09 14:45:30 -0300

    Implemented functions

--- src/functions/intersect.js
+++ src/functions/intersect.js
@@ -3,11 +3,20 @@
 const { parseEvent, intersect } = require('../helpers/utils');
 
 function processEvent(arrays) {
- 
+  if (!arrays || arrays.length != 2) {
+    return {
+      message: 'Invalid event, please refer to https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html',
+      reason: 'Expected two records with an array in each record body'
+    };
+  }
+
+  const result = intersect(...arrays);
+
+  return { message: 'Parsed with success', result };
 }
 
 async function handler(event) {
-  
+  return processEvent(parseEvent(event));
 }
 
 module.exports = {
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,9 +1,17 @@
 function intersect(arr1, arr2) {
-
+    return arr1
+        .filter(item => arr2.includes(item))
+        .sort((a, b) => a > b);
 }
 
 function parseEvent(event) {
+    if (!event || !event.Records) {
+        return [];
+    }
 
+    return event.Records
+        .map(record => record.body)
+        .filter(body => Array.isArray(body));
 }
 
 module.exports = {
