075f9180d5c66af826515b94975d91c49720fd4c
Raul Souza <raul.3k@gmail.com>
2019-07-10 17:44:59 -0300

    Added filter to remove duplicated valeus

--- src/arrayCommonElements.js
+++ src/arrayCommonElements.js
@@ -1,5 +1,5 @@
 const arrayCommonElements = (array1, array2) =>
-    array1.filter( value => array2.includes(value) )
-    .sort((a,b) => a-b);
+    [... new Set(array1.filter( value => array2.includes(value) )
+    .sort((a,b) => a-b))];
 
 module.exports = arrayCommonElements;
