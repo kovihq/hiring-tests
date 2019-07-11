bc82a5039d4cc59e4da0e82b97c7af05c983fed4
Raul Souza <raul.3k@gmail.com>
2019-07-10 23:01:33 -0300

    Added command to pull image on run

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+docker pull raulsms/test-queue-message-array
 docker run -p 3000:3000 -d raulsms/test-queue-message-array
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker pull raulsms/test-queue-message-array
 docker run -p 3000:3000 -d raulsms/test-queue-message-array
