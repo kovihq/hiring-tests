691fc57d74f06c9dfc0f1aeb814d8ddc622e64cc
Lucas Badico <lgsantana1@stefanini.com>
2019-07-27 02:36:24 -0300

    refatored tests to parameters tests paradigm

--- src/services/__tests__/processMessage.spec.js
+++ src/services/__tests__/processMessage.spec.js
@@ -1,47 +1,58 @@
 const processMessage = require('../processMessage')
 
 describe('processMessage function', () => {
-    it('should return a array', () => {
-        const message = {
-            messageId:'18-3-5-6-22-1-2-32.1-9-12-14',
+    const parameters = [
+        {
+            description: 'should return a array',
             input: [
                 [18,3,5,6,22,1,2,32],
                 [1, 9, 12, 14]
-            ]
-        }
-        expect(processMessage(message)).toBeInstanceOf(Array)
-    })
+            ],
+            assertion: 'toBeInstanceOf',
+            params: [Array] 
 
-    it('should return unique items', () => {
-        const message = {
-            messageId:'18-3-5-6-22-1-2-32.1-9-12-14',
+        },
+        {
+            description: 'should return unique items',
             input: [
                 [1],
                 [1]
-            ]
-        }
-        expect(processMessage(message)).toMatchObject([1])
-    })
+            ],
+            assertion: 'toMatchObject',
+            params: [[1]] 
+        },
 
-    it('should return the intersected items', () => {
-        const message = {
-            messageId:'18-3-5-6-22-1-2-32.1-9-12-14',
+        {
+            description: 'should return only the intersected items',
             input: [
                 [1,2,3],
                 [1, 5, 6]
-            ]
-        }
-        expect(processMessage(message)).toMatchObject([1])
-    })
+            ],
+            assertion: 'toMatchObject',
+            params: [[1]] 
+        },
 
-    it('should the array be sorted', () => {
-        const message = {
-            messageId:'18-3-5-6-22-1-2-32.1-9-12-14',
+        {
+            description: 'should the array be sorted',
             input: [
                 [2,3],
                 [3, 2, 5, 6]
-            ]
+            ],
+            assertion: 'toMatchObject',
+            params: [[2, 3]] 
+        },
+
+    ]
+    parameters.map(({
+        description,
+        input,
+        assertion,
+        params
+    }) => it(description, () => {
+         const message = {
+            messageId:'18-3-5-6-22-1-2-32.1-9-12-14',
+            input,
         }
-        expect(processMessage(message)).toMatchObject([2, 3])
-    })
+        expect(processMessage(message))[assertion](...params)
+    }))
 })
