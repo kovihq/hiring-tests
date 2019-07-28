f8f336fe2eb7ac35d6bb14d5a13fb8a709ca3694
Lucas Badico <lucasgomesdesantana@gmail.com>
2019-07-28 20:38:52 -0300

    fixed file error between mac and linux

--- src/ioc/index.js
+++ src/ioc/index.js
@@ -1,10 +1,11 @@
 // const awsXRay = require('aws-xray-sdk')
 // const AWS = awsXRay.captureAWS(require('aws-sdk'))
 const container = require('@spark/services-container')
-const Presenter = require('../helpers/Presenter')
 const { default: DynamoDBORMProvider } = require('@spark/dynamodborm/lib/provider')
 
-const presenter = require('../helpers/presenter')
+
+const Presenter = require('../helpers/Presenter.class');
+const presenter = require('../helpers/presenter.instance');
 const messageORM = require('../orm')
 
 const saveMessageInput = require( '../services/saveMessageInput')
